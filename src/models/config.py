Mega commit: - Fix hinge loss calculation - Add temperature to scores - Fix beam search with scorer

src/models/config.py
@@ -8,7 +8,7 @@ model_config = {
     'min_lr': 1e-7,  # minimum allowable value of lr
     'task': 'rec',  # mt/dialog/rec/dialog-rec
     # model-specific hyperparams
-    'anneal_till': 1500,  # for vae
+    'anneal_till': 350,  # for vae
     'x0': 5500,  # for vae
     'k': 5e-3,  # slope of the logistic annealing function (for vae)
     'anneal_type': 'tanh',  # for vae {tanh, logistic, linear}
@@ -39,7 +39,7 @@ model_config = {
     'MAX_LENGTH': 30,  # Max length of a sentence
 
     # run-time conf
-    'device': 'cpu' if torch.cuda.is_available() else 'cpu',  # gpu_id ('x' for multiGPU mode)
+    'device': 'cuda:0' if torch.cuda.is_available() else 'cpu',  # gpu_id ('x' for multiGPU mode)
     'wemb_type': 'w2v',  # type of word embedding to use: w2v/fasttext
     'lang_pair': 'en-en',  # src-target language pair
     'use_scheduler': True,  # half lr every 3 non-improving batches
@@ -50,15 +50,14 @@ model_config = {
     'filter_lang': 'english',
     'filter_genre': {'Pop', 'Rock', 'Alternative', 'Dance',
                      'Metal', 'Country', 'Electro', 'R&B'},
-    # 'filter_genre': {'Metal'},
     'embedding_dim': 300,
     'max_songs': 200,  # maximum songs to consider per genre
     'bidirectional': True,
     'use_melfeats?': False,  # whether to use already extracted img features or use calculate them on the fly while encoding
     'use_embeddings?': True,
     'generate_spectrograms': False,
-    'pretrained_model': 'vae-1L-bilstm-11',  # {'rec/vae-1L-bilstm-40', False},
-    'pretrained_scorer': 'bimodal_scorer-1L-bilstm-0',
+    'pretrained_model': 'vae-1L-bilstm-68',  # {'vae-1L-bilstm-11', False},
+    'pretrained_scorer': 'bimodal_scorer-1L-bilstm-0',  # {'bimodal_scorer-1L-bilstm-0', False}
     'save_dir': 'saved_models/',
     'data_dir': 'data/processed/',
     'model_code': 'vae',  # bimodal_scorer/bilstm_scorer/dae/vae/clf
@@ -69,7 +68,7 @@ model_config = {
     # 'spectrograms': '/home/d35kumar/Github/lyrics_generation/data/processed/spectrograms_split/'
     'dali_path': '/collection/gsahu/ae/lyrics_generation/data/raw/DALI_v1.0/',
     'dali_audio': '/collection/gsahu/ae/lyrics_generation/data/raw/ogg_audio/',  # Path to store dali audio files
-    'dali_lyrics': '/collection/gsahu/ae/lyrics_generation/data/raw/lyrics.txt',  # Path to load dali lyrics
+    'dali_lyrics': '/collection/gsahu/ae/lyrics_generation/data/raw/small_lyrics_info.txt',  # Path to load dali lyrics
     'split_spec': '/collection/gsahu/ae/lyrics_generation/data/processed/spectrograms_split/',  # Path to load dali lyrics
     # 'dali_path': '/home/gsahu/code/lyrics_generation/data/raw/DALI_v1.0/',
     # 'dali_audio': '/home/gsahu/code/lyrics_generation/data/raw/dali_audio/',  # Path to store dali audio files

src/models/dae.py
@@ -1,5 +1,6 @@
 import torch
 import random
+from tqdm import tqdm
 from torch import nn, optim
 from models import attn_module
 from models import scoring_functions
@@ -84,26 +85,38 @@ class AutoEncoder(nn.Module):
         scorer.load_state_dict(
             torch.load('{}{}'.format(self.config['save_dir'],
                                      self.config['pretrained_scorer']),
-                       map_location=self.config['device'])['model'])
-        return scorer
+                       map_location=self.device)['model'])
+        return scorer.to(self.device)
 
-    def _scoring_function(self, old_scores, candidates,
-                          mel_specs, scorer_emb_wts):
+    def _get_scores(self, old_scores, candidates, mel_specs, scorer):
         """
         updates scores for all the candidates by measuring their compatability
         with the respective spectrograms
         candidates: (t, bs, k, vocab_size) where t is the current time step
         mel_specs: (bs, 3, 224, 224)
         """
-        t, bs, k, v = candidates.shape
-        scorer = self._load_scorer(scorer_emb_wts, 'bimodal')
+        # t, bs, k, v = candidates.shape
+        # n_candidates = bs*k*v
+        t, n_candidates = candidates.shape
+        k = self.beam_size
+        v = self.vocab.size
+        # candidates = candidates.view(t, bs*k*v).long()
+        candidates = candidates.long()
+        scores = torch.empty(0).to(self.device)
         # compatibility scores -> (bs, beam_size*v)
-        scores = scorer({
-            'lyrics_seq': candidates.view(t, bs*k*v).long(),
-            'mel_spec': mel_specs.repeat(k*v, 1, 1, 1)
-            })
+        # print('Passing candidates through {}'.format(type(scorer).__name__))
+        continue_count = 0
+        for iter in tqdm(range(0, n_candidates, self.config['batch_size'])):
+            curr_candidates = candidates[:, iter:iter+self.config['batch_size']]
+            curr_mel_specs = mel_specs[iter // (k*v)].unsqueeze(0).repeat(
+                curr_candidates.shape[1], 1, 1, 1)
+            curr_scores = scorer({
+                'lyrics_seq': curr_candidates,
+                'mel_spec': curr_mel_specs
+                })[:, 1]  # get scores for class 1
+            scores = torch.cat((scores, curr_scores))
         # adjust scores to make it broadcastable wrt old_scores
-        scores = scores.view(bs, self.beam_size).unsqueeze(0).unsqueeze(-1)
+        scores = scores.view(-1, k, v).unsqueeze(0)
         # return updated scores -> (t, bs, k, v)
         return (old_scores * torch.exp(scores/self.config['scorer_temp']))
 
@@ -152,9 +165,26 @@ class AutoEncoder(nn.Module):
         max_y_len, bs = y.shape
         vocab_size = self.vocab.size
 
-        # tensor to store decoder outputs
-        decoder_outputs = torch.zeros(max_y_len, bs*self.beam_size,
-                                      vocab_size).to(self.device)
+        if y_specs is not None:
+            scorer = self._load_scorer(scorer_emb_wts, 'bimodal')
+            # tensor to maintain the already guess subsequence for k beams
+            # this tensor is intialized such that it has all vocab tokens as
+            # candidates for the k beams but at each time step, we will
+            # replace all of them with a single token for each beam
+            # candidate_subseq => (max_y_len, bs*k*vocab)
+            scorer_vocab = scorer_emb_wts.shape[0]
+            candidate_subseq = torch.arange(scorer_vocab).repeat(
+                bs*self.beam_size).unsqueeze(0).repeat(max_y_len, 1).to(
+                self.device)
+            candidate_subseq[0] = torch.ones(
+                scorer_vocab*bs*self.beam_size)*self.sos_idx
+            # tensor to store decoder outputs
+            decoder_outputs = (torch.ones(max_y_len, bs*self.beam_size,
+                               scorer_vocab)*self.sos_idx).to(self.device)
+        else:
+            # tensor to store decoder outputs
+            decoder_outputs = (torch.ones(max_y_len, bs*self.beam_size,
+                               vocab_size)*self.sos_idx).to(self.device)
 
         # Reconstruct the hidden vector from z
         # if #dec_layers > #enc_layers, use currently obtained hidden
@@ -194,24 +224,30 @@ class AutoEncoder(nn.Module):
             if infer or (not do_tf):
                 if self.config['dec_mode'] == 'beam':
                     # split beam and bs dim from dec_outputs
-                    candidates = decoder_outputs[:t].view(t, bs,
+                    new_logits = decoder_outputs[:t].view(t, bs,
                                                           self.beam_size,
-                                                          self.vocab.size)
+                                                          vocab_size)
                     # run a softmax on the last dimension
-                    new_logits = torch.log_softmax(candidates, dim=-1)
+                    new_logits = torch.log_softmax(new_logits, dim=-1)
                     if y_specs is not None:
-                        new_logits = self._scoring_function(new_logits,
-                                                            candidates,
-                                                            y_specs,
-                                                            scorer_emb_wts)
+                        candidates = candidate_subseq[:t+1]
+                        new_logits = self._get_scores(new_logits,
+                                                      candidates,
+                                                      y_specs,
+                                                      scorer)
                     # extract probabilities of the tokens and flatten logits
                     new_logits = new_logits[-1].squeeze(0).view(bs, -1)
                     # new_logits => (bs, beam_size*vocab_size)
                     # .topk returns scores and tokens of shape (bs, k)
                     # the modulo operator is required to get real token ids
                     # as its range would be beam_size*vocab_size otherwise
-                    output = new_logits.topk(k=self.beam_size, dim=-1)[1].view(-1) % self.vocab.size
+                    output = new_logits.topk(k=self.beam_size, dim=-1)[1].view(
+                        -1) % self.vocab.size
                     # output => (bs*beam_size)
+                    if y_specs is not None:
+                        # replace all the tokens at time t with the guessed
+                        # token for k beams
+                        candidate_subseq[t] = output.repeat(vocab_size)
                 else:
                     # output.max(1) -> (scores, tokens)
                     # doing a max along `dim=1` returns logit scores and

src/models/scoring_functions.py
@@ -126,6 +126,7 @@ class BiModalScorer(nn.Module):
         self.config = config
         self.dropout = config['dropout']
         self.embedding_wts = embedding_wts
+        self.output_dim = 2  # we want to return a score
         self.embedding = nn.Embedding.from_pretrained(embedding_wts,
                                                       freeze=False)
         self.embedding_dropout = nn.Dropout(config['dropout'])
@@ -159,21 +160,25 @@ class BiModalScorer(nn.Module):
         for p in self.img_encoder.classifier.parameters():
             p.requires_grad = True
 
-        # we need to bring music and lyrics features to a same dimension
-        # to calculate the loss (or we can append zeros to shorter ones
-        # but that's more like a hack)
-        self.to_lyrics_dim = nn.Linear(
-            self.img_encoder.classifier[6].out_features,
-            self.pf*config['hidden_dim'])
+        # We will concatenate img and lyrics features, so input size becomes
+        # 1000 (from vgg16) + 2*hidden_dim (from rnn)
+        self.final_in_features = \
+            self.img_encoder.classifier[6].out_features + \
+            self.pf*self.config['hidden_dim']
+
+        self.out = nn.Sequential(
+            nn.Linear(self.final_in_features, self.final_in_features // 2),
+            nn.ReLU(inplace=True),
+            nn.Linear(self.final_in_features // 2, self.output_dim))
+
+        self.softmax = nn.LogSoftmax(dim=1)
 
     def pool(self, rnn_output):
         pool_func = F.avg_pool1d if self.avg_pool else F.max_pool1d
         return pool_func(rnn_output, rnn_output.size(2)).squeeze(2)
 
-    def score(self, music, lyrics):
-        # l2 => (bs, pf*lyrics_dim)
-        l2 = F.mse_loss(music, lyrics, reduction='none')
-        return torch.sigmoid(torch.mean(l2, dim=1))  # (bs)
+    def fusion(self, music, lyrics):
+        return torch.cat((music, lyrics), dim=-1)
 
     def encoder(self, embedded):
         if self.unit == 'lstm':
@@ -188,17 +193,16 @@ class BiModalScorer(nn.Module):
         music_melspec = x['mel_spec']
         # max_sequence_length, batch_size, embedding_dim
         lyrics_embeddings = self.embedding(x['lyrics_seq'])
-        print(lyrics_embeddings.shape)
         if self.config['use_melfeats?']:
             raise NotImplementedError('Using direct image features not supported yet.')
         else:
             # (batch_size, 1000)
             music_features = self.img_encoder(music_melspec)
 
-        bs, music_dims = music_features.shape
         lyrics_output, lyrics_hidden = self.encoder(lyrics_embeddings)
         lyrics_pool = self.pool(lyrics_output.permute(1, 2, 0))
-        return self.score(self.to_lyrics_dim(music_features), lyrics_pool)
+        # bs, output_dim
+        return self.softmax(self.out(self.fusion(music_features, lyrics_pool)))
 
 
 class GenreClassifier(BiModalScorer):

src/predict.py
@@ -6,6 +6,7 @@ from models.config import model_config as conf
 from train_model import load_vocabulary, get_embedding_wts
 from models import dae, vae
 import h5py
+import skimage
 import numpy as np
 
 
@@ -36,18 +37,25 @@ def get_z(x, x_lens, model):
     return model._reparameterize(mu, log_sigma)
 
 
+def show_img(data, name):
+    skimage.io.imsave(name, data)
+
+
 def get_specs(n):
     with open('data/processed/bimodal_scorer/spec_array.pkl', 'rb') as f:
         spec_array = pickle.load(f)
     # randomly choose n spec_ids
     spec_ids = np.random.choice(list(spec_array.keys()), size=n)
+    print(spec_ids)
+    # for k in spec_ids:
+    #     show_img(np.array(spec_array[k], dtype=np.uint8), str(k) + '.png')
     specs = [[spec_array[k]] for k in spec_ids]
-    return torch.tensor(specs).float().view(-1, 224, 224, 3).permute(0, 3, 1, 2).contiguous()
+    return torch.tensor(specs).float().view(-1, 224, 224, 3).permute(0, 3, 1, 2).contiguous().to(conf['device'])
 
 
 def load_scorer_embeddings():
     with h5py.File('data/processed/bimodal_scorer/english_w2v_filtered.hd5', 'r') as f:
-        return torch.from_numpy(np.array(f['data'])).float()
+        return torch.from_numpy(np.array(f['data'])).float().to(conf['device'])
 
 
 def main():
@@ -68,20 +76,22 @@ def main():
             '{}{}'.format(conf['save_dir'], conf['pretrained_model']),
             map_location=device)['model'])
 
+    model = model.to(device)
+
     with torch.no_grad():
-        print('\n### Random Sampling ###:\n')
-        random_sampled = translate(vocab, model._random_sample(9))
-        for s in random_sampled:
-            print(s)
-        with open('random_sampled_sf.txt', 'w') as f:
-            f.write('\n'.join(random_sampled))
+        # print('\n### Random Sampling ###:\n')
+        # random_sampled = translate(vocab, model._random_sample(9))
+        # for s in random_sampled:
+        #     print(s)
+        # with open('random_sampled_sf.txt', 'w') as f:
+        #     f.write('\n'.join(random_sampled))
 
-        _, val_pairs, _ = preprocess.prepare_data(conf)
-        s1 = random.choice(val_pairs)[0]
-        s2 = random.choice(val_pairs)[0]
+        # _, val_pairs, _ = preprocess.prepare_data(conf)
+        # s1 = random.choice(val_pairs)[0]
+        # s2 = random.choice(val_pairs)[0]
 
-        x1, x1_lens, _ = preprocess._btmcd(vocab, [(s1, s1)], conf)
-        x2, x2_lens, _ = preprocess._btmcd(vocab, [(s2, s2)], conf)
+        # x1, x1_lens, _ = preprocess._btmcd(vocab, [(s1, s1)], conf)
+        # x2, x2_lens, _ = preprocess._btmcd(vocab, [(s2, s2)], conf)
 
         # print('\n### Linear Interpolation ###:\n')
         # print(s1)
@@ -95,9 +105,10 @@ def main():
         # print(s2)
 
         print('\n### Testing VAE+Scoring function ###:\n')
-        n = 2
+        n = 1
         scorer_emb_wts = load_scorer_embeddings()
-        random_sampled = translate(vocab, model._random_sample(n, get_specs(n),
+        specs = get_specs(n)
+        random_sampled = translate(vocab, model._random_sample(n, specs,
                                                                scorer_emb_wts))
         for s in random_sampled:
             print(s)

src/train_scorer.py
@@ -99,7 +99,7 @@ def main():
         print('Genres: {}'.format(', '.join(sorted(list(conf['filter_genre'])))))
     model = model.to(device)
     if 'scorer' in conf['model_code']:
-        criterion = nn.HingeEmbeddingLoss()  # to train scoring function
+        criterion = nn.NLLLoss()  # to train scoring function
     else:
         criterion = nn.CrossEntropyLoss(reduction='sum')  # to train genre classifier
     optimizer = optim.Adam(model.parameters(), lr=conf['lr'])
@@ -162,17 +162,7 @@ def main():
                     x_val = preprocess._btmcd(vocab, iter_pairs, conf)
 
                 predictions = model(x_val)
-
-                # Since we are using hinge loss for scorers the outputs would
-                # either 1 or -1. The block below gets the generated label
-                # for scorers case
-                if 'scorer' in conf['model_code']:
-                    preds = np.array(list(
-                        map(lambda e: 1 if e >= 0.5 else -1,
-                            predictions.cpu().numpy())))
-                else:
-                    preds = torch.argmax(predictions, dim=1).cpu().numpy()
-
+                preds = torch.argmax(predictions, dim=1).cpu().numpy()
                 loss = criterion(predictions, y_train[iter: iter + predictions.shape[0]])
                 epoch_loss.append(loss.item())
                 generated = np.concatenate((generated, preds))
@@ -206,16 +196,7 @@ def main():
 
                     predictions = model(x_test)
 
-                    # Since we are using hinge loss for scorers the outputs would
-                    # either 1 or -1. The block below gets the generated label
-                    # for scoreres case
-                    if 'scorer' in conf['model_code']:
-                        preds = np.array(list(
-                            map(lambda e: 1 if e >= 0.5 else -1,
-                                predictions.cpu().numpy())))
-                    else:
-                        preds = torch.argmax(predictions, dim=1).cpu().numpy()
-
+                    preds = torch.argmax(predictions, dim=1).cpu().numpy()
                     loss = criterion(predictions, y_train[iter: iter + predictions.shape[0]])
                     epoch_loss.append(loss.item())
                     generated = np.concatenate((generated, preds))

src/utils/preprocess.py
@@ -14,6 +14,8 @@ from tqdm import tqdm
 import DALI as dali_code
 from torch.nn.utils.rnn import pad_sequence
 from sklearn.model_selection import train_test_split
+import scipy
+from scipy import misc
 
 import matplotlib.pyplot as plt
 
@@ -92,6 +94,40 @@ def process_bimodal(config):
     return dataset
 
 
+def process_lm(config):
+    """
+    This function creates a tsv file with lyrics and their corresponding genre
+    """
+    lm_data = ''
+    print('Loading DALI')
+    dali_data = dali_code.get_the_DALI_dataset(config['dali_path'],
+                                               skip=[], keep=[])
+    file_ids = os.listdir(config['dali_path'])
+    genre_list = sorted(list(config['filter_genre']))
+    for f_id in file_ids:
+        if f_id in ['info', 'audio.zip']:
+            continue
+        # f_id is of the format id.gz. remove the gz part
+        f_id = f_id.split('.')[0]
+        annotations = dali_data[f_id].annotations
+        line_level = annotations['annot']['lines']
+        info = dali_data[f_id].info
+        if 'genres' not in info['metadata']:
+            continue
+        if info['metadata']['language'] != config['filter_lang']:
+            continue
+        genre = info['metadata']['genres']
+        if genre == []:
+            continue
+        genre = genre[0]
+        if genre not in genre_list:
+            continue
+        for l in line_level:
+            lm_data += '{}\t{}\t{}\n'.format(l['text'], genre, f_id)
+    with open('big_lyrics_info.txt', 'w') as f:
+        f.write(lm_data)
+
+
 def read_spectrogram_batch(spectrogram_paths):
     # remove alpha dimension and resize to 224x224
     return [skimage.transform.resize(s[:, :, :3], (224, 224, 3))
@@ -100,7 +136,7 @@ def read_spectrogram_batch(spectrogram_paths):
 
 def read_spectrogram(spectrogram_path):
     # remove alpha dimension and resize to 224x224
-    return skimage.transform.resize(
+    return scipy.misc.imresize(
         skimage.io.imread(spectrogram_path)[:, :, :3], (224, 224, 3)
         )
 
@@ -156,8 +192,15 @@ def process_ae(config):
     print('Loading lyrics dataset')
     with open(config['dali_lyrics'], 'r') as f:
         lyrics_info = f.readlines()
+    genre_count = {}
     for l in lyrics_info:
-        line, spec_path = l.split('\t')
+        line, genre, _ = l.split('\t')
+        if genre not in genre_count:
+            genre_count[genre] = 0
+        genre_count[genre] += 1
+        # take only `max_songs` number of songs for a genre
+        if genre_count[genre] > config['max_songs']:
+            continue
         dataset.append(line)
     return list(set(dataset))
 
@@ -171,11 +214,8 @@ def process_clf(config):
     with open(config['dali_lyrics'], 'r') as f:
         lyrics_info = f.readlines()
     genre_list = sorted(list(config['filter_genre']))
-    genres = []
-    spec_paths = []
-    for l in lyrics_info[:10000]:
+    for l in lyrics_info:
         line, spec_path = l.split('\t')
-        spec_paths.append(spec_path)
         # spec_path of the form:
         # {path_to_split_spectrograms}/8d2bea941a11497a984e50de3119b4d4_0.ogg
         # below command splits by '/', keep the last element, then splits by
@@ -183,7 +223,6 @@ def process_clf(config):
         spec_id = spec_path.split('/')[-1].split('_')[0]
         info = dali_data[spec_id].info
         genre = info['metadata']['genres'][0]  # consider 1st as the major one
-        genres.append(genre)
         # Note: this line whould throw an error if genre is not present in
         # genre_list but this shouldn't happen since the lyrics were filtered
         # using the same config. In other words, it's like a sanity check.
@@ -192,11 +231,6 @@ def process_clf(config):
                   'genre_id': genre_id,
                   'genre': genre}
         dataset.append(sample)
-    with open('genres.txt', 'w') as f:
-        f.write('\n'.join(genres))
-    with open('specs.txt', 'w') as f:
-        f.write('\n'.join(spec_paths))
-    return None
     return dataset
 
 
@@ -328,7 +362,7 @@ def read4bimodal(dataset):
         mel_paths.append(v['spec_id'])
 
     pairs = list(zip(lyrics_list, mel_paths))
-    y = [1] * len(pairs) + [-1] * len(pairs)
+    y = [1] * len(pairs) + [0] * len(pairs)
 
     # neg sampling
     total_samples = len(dataset)